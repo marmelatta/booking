027f48505312231313820644d2337c5db80c1ae1
marmelatta <testandroidapp@yandex.ru>
2022-09-02 13:57:18 +0500

    initial

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017', {
+      dbName: 'booking',
+      user: 'admin',
+      pass: 'password',
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/entities/hotel-room.entity.ts
@@ -0,0 +1,27 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { Document } from 'mongoose';
+
+export type HotelRoomDocument = HotelRoom & Document;
+
+@Schema()
+export class HotelRoom {
+  @Prop({ isRequired: true })
+  hotelId: string;
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Hotel',
+    required: true,
+  })
+  @Prop()
+  description: string;
+
+  @Prop()
+  images: string[];
+
+  @Prop({ isRequired: true })
+  isEnabled: boolean;
+}
+
+export const HotelRoomSchema = SchemaFactory.createForClass(HotelRoom);
