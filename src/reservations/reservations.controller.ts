ff6b6cf8d7a14f671a2459e1f4cf841169670470
marmelatta <testandroidapp@yandex.ru>
2022-09-15 19:08:57 +0500

    reservationController

--- src/reservations/reservations.controller.ts
+++ src/reservations/reservations.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { ReservationsService } from './reservations.service';
+import { IReservationDto } from './dto/reservation.dto';
+import { ReservationSearchOptions } from './dto/reservation-search-options.dto';
+import { ID } from '../types/types';
 
 @Controller('reservations')
-export class ReservationsController {}
+export class ReservationsController {
+  constructor(private reservationsService: ReservationsService) {}
+
+  @Post()
+  async addReservation(@Body() data: IReservationDto) {
+    return await this.reservationsService.addReservation(data);
+  }
+
+  @Get()
+  async getReservations(@Query() filter: ReservationSearchOptions) {
+    return await this.reservationsService.getReservations(filter);
+  }
+
+  @Delete(':id')
+  async removeReservation(@Param() id: ID) {
+    return await this.reservationsService.removeReservation(id);
+  }
+}
