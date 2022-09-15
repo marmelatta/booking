f42fb43718a5ffd77263a903910a8f89b7d6e18e
marmelatta <testandroidapp@yandex.ru>
2022-09-15 19:18:16 +0500

    add user foreign key to reservation

--- src/reservations/entities/reservation.entity.ts
+++ src/reservations/entities/reservation.entity.ts
@@ -3,11 +3,19 @@ import * as mongoose from 'mongoose';
 import { Hotel } from '../../hotels/entities/hotel.entity';
 import { HotelRoom } from '../../hotels/entities/hotel-room.entity';
 import { Document } from 'mongoose';
+import { User } from '../../users/entities/user.entity';
 
 export type ReservationDocument = Reservation & Document;
 
 @Schema()
 export class Reservation {
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  })
+  user: User;
+
   @Prop({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Hotel',
